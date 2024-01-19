d7e0fd4f270fa9a97a701766804a8f99aaff5cfd
Suraj Yerramilli <surajyerramilli@gmail.com>
2024-01-19 08:59:52 -0600

    removing redundant code

--- bnn_model.py
+++ bnn_model.py
@@ -20,7 +20,7 @@ class FlaxMLP(nn.Module):
         x = nn.Dense(1)(x)
         return x
     
-def bnn_model(
+def numpyro_bnn_model(
     x, y=None,
     n_layers:int=1,
     hidden_dim:int=25,
@@ -50,11 +50,17 @@ def bnn_model(
     # get dimensions
     n, d = x.shape
 
+    def get_prior(name, shape):
+        if 'bias' in name:
+            return Normal(0, 1 / jnp.sqrt(gamma)).expand(shape)
+
+        return Normal(0, 1 / jnp.sqrt(gamma * shape[0])).expand(shape)
+
     # Neural network
     mlp = random_flax_module(
         name="mlp",
         nn_module=FlaxMLP(n_layers, hidden_dim),
-        prior=Normal(0, 1 / jnp.sqrt(gamma)),
+        prior=get_prior,
         input_shape=(d,)
     )
 
@@ -70,59 +76,5 @@ def bnn_model(
         
         mean_y = numpyro.deterministic('y_pred', mlp(batch_x).ravel())
         numpyro.sample(
-            "y", Normal(mean_y, 1 / jnp.sqrt(prec_obs)).to_event(1), obs=batch_y
-        )
-
-
-
-def numpyro_bnn_model(
-    x, y=None, 
-    hidden_dim:int=50,
-    subsample_size:int=100
-):
-    '''
-    Single hidden layer Bayesian neural network model with priors
-    on weights and biases as described in Liu, Q., & Wang, D. (2016). 
-    Stein variational gradient descent: A general purpose Bayesian 
-    inference algorithm. Advances in neural information processing 
-    systems, 29.
-
-    Args:
-        x: Input data.
-        y: Target data.
-        hidden_dim: Number of hidden units in the neural network.
-        subsample_size: Size of the subsample used for inference.
-    '''
-
-    # hyperprior for the precision of the weights and biases
-    gamma = numpyro.sample('gamma', Gamma(1.0, 0.1))
-
-    # Set up dimensions
-    n, d = x.shape
-
-    # hidden layer
-    b1 = numpyro.sample('b1', Normal(0, 1 / jnp.sqrt(gamma)).expand([hidden_dim]))
-    w1 = numpyro.sample('w1', Normal(0, 1 / jnp.sqrt(gamma)).expand([d, hidden_dim]))
-
-    # output layer
-    w2 = numpyro.sample('w2', Normal(0, 1 / jnp.sqrt(gamma)).expand([hidden_dim, 1]))    
-    b2 = numpyro.sample('b2', Normal(0, 1 / jnp.sqrt(gamma)).expand([1]))
-
-    # precision parameter for observations
-    prec_obs = numpyro.sample('prec_obs', Gamma(1.0, 0.1))
-
-    # observation model
-    with numpyro.plate("data", n, subsample_size=subsample_size, dim=-1):
-        batch_x = numpyro.subsample(x, event_dim=1)
-        if y is not None:
-            batch_y = numpyro.subsample(y, event_dim=0)
-        else:
-            batch_y = y
-        
-        mean_y = numpyro.deterministic(
-            'y_pred', (jnp.maximum(batch_x @ w1 + b1, 0) @ w2 + b2).ravel()
-        )
-
-        numpyro.sample(
-            "y", Normal(mean_y, 1 / jnp.sqrt(prec_obs)), obs=batch_y
+           "y", Normal(mean_y, 1 / jnp.sqrt(prec_obs)), obs=batch_y
         )
